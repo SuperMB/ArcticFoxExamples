fbe973ed8d90333f7fa71e37661883919b30228d
Michael Bass <littlebass09@tamu.edu>
2025-02-04 10:43:32 -0800

    MB updated all todos

--- PandasExamples/VisualizeDefaults.py
+++ PandasExamples/VisualizeDefaults.py
@@ -41,6 +41,11 @@ weatherDf = pd.read_csv('weather.csv')
 # With the configurations below, VisualizeDefaults makes
 # all Visualize seeds in the workspace show all of the columns
 # and show 20 rows.
+#
+# Note: The seed is commented out below since it is a global
+# seed. If it was uncommented, it would impact every Visualize
+# seed in the workspace. 
+#
 # Seed being used: #> VisualizeDefaults --allColumns --count 20 
 # ******************************************************
 # ******************************************************
