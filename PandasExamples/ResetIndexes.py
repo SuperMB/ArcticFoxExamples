05fa36e43ac292ddae1c4c7ab41f06fba4c43071
Michael Bass <littlebass09@tamu.edu>
2025-02-05 12:33:11 -0800

    MB Adding more examples

---
+++ PandasExamples/ResetIndexes.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+import numpy as np 
+# ******************************************************
+# ******************************************************
+
+#> Data BankTransactions.csv 
+bankTransactionsDf = pd.read_csv('BankTransactions.csv')
+bankTransactionsDf['TransactionDate'] = pd.to_datetime(bankTransactionsDf['TransactionDate'])
+bankTransactionsDf['PreviousTransactionDate'] = pd.to_datetime(bankTransactionsDf['PreviousTransactionDate']) 
+
+
+
+1:(013905127610713076) 
+
