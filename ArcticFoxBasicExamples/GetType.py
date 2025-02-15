206d64103a2d5418797c4922807df74e74eb33c1
Michael Bass <littlebass09@tamu.edu>
2025-02-14 18:35:19 -0800

    MB Added 1 more example

---
+++ ArcticFoxBasicExamples/GetType.py
@@ -0,0 +1,31 @@
+
+
+
+# Example 1
+# Get the type of the last variable
+#
+# Seed being used: #> GetType 
+# ******************************************************
+# ******************************************************
+
+values = [1, 2, 3, 4]
+
+#> GetType 
+valuesType = type(values) 
+
+
+
+# Example 2
+# Get the type of the last variable and print the type
+#
+# Seed being used: #> GetType 
+# ******************************************************
+# ******************************************************
+
+newValues = [5, 6, 7, 8]
+
+#> GetType --print 
+newValuesType = type(newValues)
+print(newValuesType) #)1 
+##*** <class 'list'>
+
