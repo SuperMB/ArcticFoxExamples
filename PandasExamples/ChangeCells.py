4627ec7763383b80235ca5694d2a9e2366e6d39c
Michael Bass <littlebass09@tamu.edu>
2025-07-13 17:36:28 -0500

    MB Example titles working

--- PandasExamples/ChangeCells.py
+++ PandasExamples/ChangeCells.py
@@ -34,7 +34,7 @@ print(bankTransactionsDf.head(n=6)) #)1
 ##*** 4      TX000005   AC00411              13.45 2023-10-16 17:51:24          Credit        Atlanta  D000308    65.164.3.100       M091  Online           26            Student                  198              1         7429.40     2024-11-04 08:06:39
 ##*** 5      TX000006   AC00393              92.15 2023-04-03 17:15:01           Debit  Oklahoma City  D000579  117.67.192.211       M054     ATM           18            Student                  172              1          781.68     2024-11-04 08:06:36
 
-#> ChangeCells --rows 2 4 5 --columns AccountID --value Inactive --example 
+#> ChangeCells --rows 2 4 5 --columns AccountID --value Inactive --exampleTitle Change Cell by Row and Column --example Change specific rows within one or more columns to a new value. In this case, in the column AcountID we are changing rows with index 2, 4, and 5 to Inactive to mark that those accounts are no longer used. 
 bankTransactionsDf.loc[[2, 4, 5], 'AccountID' ] = 'Inactive' 
 
 #> Visualize --count 6 
@@ -64,7 +64,7 @@ print(bankTransactionsDf.head()) #)3
 ##*** 3      TX000004   AC00070             184.50 2023-05-05 16:32:11           Debit    Raleigh  D000187  200.13.225.150       M002  Online           26            Student                   25              1         8569.06     2024-11-04 08:09:06
 ##*** 4      TX000005  Inactive              13.45 2023-10-16 17:51:24          Credit    Atlanta  D000308    65.164.3.100       M091  Online           26            Student                  198              1         7429.40     2024-11-04 08:06:39
 
-#> ChangeCells --initialValues San  Diego --newValues Los  Angeles --columns Location --example 
+#> ChangeCells --initialValues San  Diego --newValues Los  Angeles --columns Location --exampleTitle Change Cells by String Match and Replace --example Modify all cells in a column that match a given string with a new value. In this case, we want to replace San Diego with Los Angeles in the column Location. If the entire cell was San Diego, than the entire cell would be change to Los Angeles. However, if the cell contained other text in addition to San Diego, the other text would remain the same, and only San Diego in the cell would change to Los Angeles. 
 bankTransactionsDf['Location'] = bankTransactionsDf['Location'].str.replace('San Diego', 'Los Angeles') 
 
 #> Visualize 
@@ -94,7 +94,7 @@ print(bankTransactionsDf.head()) #)5
 ##*** 3      TX000004   AC00070             184.50 2023-05-05 16:32:11           Debit      Raleigh  D000187  200.13.225.150       M002  Online           26            Student                   25              1         8569.06     2024-11-04 08:09:06
 ##*** 4      TX000005  Inactive              13.45 2023-10-16 17:51:24          Credit      Atlanta  D000308    65.164.3.100       M091  Online           26            Student                  198              1         7429.40     2024-11-04 08:06:39
 
-#> ChangeCells --columns TransactionType --value Cash --where _TransactionDate_ > 06-01-2023 --allMatchingRows --example 
+#> ChangeCells --columns TransactionType --value Cash --where _TransactionDate_ > 06-01-2023 --allMatchingRows --exampleTitle Change Cells That Meet Condition --example Set all cells in a column to a new value if the cell meets the desired condition. Here, the condition is that the TransactionDate in a row is after (greater than) 06-01-2023. Therefore, if the TransactionDate is after 06-01-2023, then we set the TransactionType cell to Cash in the row. This would replace then entire contents of the cell TransactionType in the row. 
 bankTransactionsDf['TransactionDate'] = pd.to_datetime(bankTransactionsDf['TransactionDate'])
 bankTransactionsDf.loc[ bankTransactionsDf['TransactionDate'] > "06 - 01 - 2023", 'TransactionType' ] = 'Cash' 
 
